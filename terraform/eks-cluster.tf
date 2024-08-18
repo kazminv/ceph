08f24b049522b51901e22c27f979e642dd5a3c68
Vasilii Kazmin <vasyakazmin171@yandex.ru>
2024-08-18 08:59:47 +0100

    works

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -93,17 +93,74 @@ module "myapp-eks" {
       key_name = "AWS-servers-key"
 
       use_custom_launch_template = false
-      launch_template_id = aws_launch_template.additional_EBS.id
-      launch_template_version = "$Latest"
+      //launch_template_id = aws_launch_template.additional_EBS.id
+      // launch_template_version = "$Latest"
       additional_tags = {
         application = "myapp"
         Terraform   = "true"
       }
     }
-
   }
-
 }
+
+//создание новой доп ROLE
+resource "aws_iam_role" "additional_eks_role" {
+  name = "additional_eks_role_for_sa_tf"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::590183654020:oidc-provider/${module.myapp-eks.oidc_provider_arn}"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${module.myapp-eks.oidc_provider_arn}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          }
+        }
+      }
+    ]
+  })
+}
+  //create a new policy
+resource "aws_iam_policy" "additional_policy" {
+  name        = "additional_eks_policy_tf"
+  description = "additional permissions for EKS"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:ListBucket",
+          "s3:GetObject",
+          "ec2:DescribeInstances"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+// привязываю policy к ROLE
+resource "aws_iam_role_policy_attachment" "attach_policy" {
+  role       = aws_iam_role.additional_eks_role.name
+  policy_arn = aws_iam_policy.additional_policy.arn
+}
+//create a service account and attach to a new ROLE
+resource "kubernetes_service_account" "ebs_csi_controller_sa" {
+  metadata {
+    name      = "ebs-csi-controller-sa"
+    namespace = "kube-system"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.additional_eks_role.arn
+    }
+  }
+}
+
+
 output "cluster_security_group_id" {
   value = module.myapp-eks.cluster_security_group_id
 }
@@ -117,23 +174,23 @@ data "aws_eks_cluster_auth" "myapp-cluster-auth" {
   depends_on = [module.myapp-eks]
 }
 provider "kubernetes" {
-  load_config_file       = "false" //не качать дефолт файл для аутент
+ // load_config_file       = "false" //не качать дефолт файл для аутент
   host                   = data.aws_eks_cluster.myapp-cluster.endpoint
   token                  = data.aws_eks_cluster_auth.myapp-cluster-auth.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.myapp-cluster.certificate_authority.0.data)
 }
 
 // Создание EC2 инстанса для Bastion
-resource "aws_instance" "bastion" {
-  ami                    = "ami-0ae8f15ae66fe8cda"
-  instance_type          = "t2.micro"
-  key_name               = "AWS-servers-key"
-  subnet_id              = element(module.myapp-vpc.public_subnets, 0) // первая публичная подсеть
-  security_groups = [aws_default_security_group.myapp-sg.id]
-  associate_public_ip_address = true
+#resource "aws_instance" "bastion" {
+  #ami                    = "ami-0ae8f15ae66fe8cda"
+  #instance_type          = "t2.micro"
+  #key_name               = "AWS-servers-key"
+  #subnet_id              = element(module.myapp-vpc.public_subnets, 0) // первая публичная подсеть
+  #security_groups = [aws_default_security_group.myapp-sg.id]
+  #associate_public_ip_address = true
 
-  tags = {
-    Name        = "Bastion"
-    Environment = "development"
-  }
-}
+  #tags = {
+    #Name        = "Bastion"
+    #Environment = "development"
+  #}
+#}
